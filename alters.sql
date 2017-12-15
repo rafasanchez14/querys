2ab34a6434b0c1e8cef6d114628972a61d48dc2e
Rafael Sanchez <rafaelsanchez814@gmail.com>
2017-12-15 12:03:28 -0430

    zonas en purebas

--- alters.sql
+++ alters.sql
@@ -73,6 +73,8 @@ alter table Material_Prueba add constraint mapu_mat
 foreign key (cod_material) references Material(cod_material);
 alter table Material_Prueba add constraint mapu_pru
 foreign key (cod_prueba) references Prueba(cod_prueba);
+alter table Material_Prueba  add constraint mp_zona
+foreign key (id_zona) references Zona (id_zona);
 
 alter table Estatus_Material add constraint est_ma
 foreign key (id_status) references Estatus (id_status);
