b8a5fb13323dad13c7b979b4d11cd165ec00f9d8
Rafael Sanchez <rafaelsanchez814@gmail.com>
2018-01-10 02:21:22 -0400

    Agregar a los inserts sustituir por la de pieza actual

---
+++ insertpieza.sql
@@ -0,0 +1,42 @@
+INSERT INTO Pieza(
+   cod_pieza, nombre_pieza, desc_pieza)
+VALUES
+(1,'Aleron de baja velocidad', 'se encargar de controlar el movimiento de balance en vuelo del avión, mediante una deflexión de manera asimétrica
+(un alerón hacia arriba y otro hacia abajo) se consigue que el avión gire sobre su eje longitudinal. Es de esta forma por la que el
+avión realiza giros laterales sin consumir una cantidad elevada de combustible y en un espacio reducido. Existen dos alerones en
+el ala: controla el giro del avión a bajo mach '),
+(2,'Aleron de alta velocidad', ' se encargar de controlar el movimiento de balance en vuelo del avión, mediante una deflexión de manera asimétrica
+(un alerón hacia arriba y otro hacia abajo) se consigue que el avión gire sobre su eje longitudinal. Es de esta forma por la que el
+avión realiza giros laterales sin consumir una cantidad elevada de combustible y en un espacio reducido. Existen dos alerones en
+el ala: controla el giro del avión a mach de crucero'),
+(3,'punta de ala','formas geométricas instaladas en el extremo del ala, su misión es reducir la resistencia inducida del ala ya que
+evita la conexión entre intradós y el extradós.'),
+(4,'carenado flaps','una cubierta externa cuya principal función consiste en reducir la resistencia al aire. Cubre las zonas de la aeronave donde potencialmente se pueda producir mayor resistencia que en otras,1​ aunque muchos modelos de aeronave están en realidad enteramente carenados.
+Estas estructuras se presentan generalmente como cubiertas ligeras que además de reducir la resistencia al avance, también proveen protección a los componentes internos que cubren.'),
+(5,'Flap Krueger', 'es un ingenio aerodinámico diseñado para aumentar la sustentación, en determinadas fases del vuelo de una aeronave. Su fin es aumentar la cuerda aerodinámica y la curvatura del perfil alar,
+  modificando la geometría del perfil de tal modo que la velocidad de entrada en pérdida durante fases concretas del vuelo, como el aterrizaje o el despegue se reduzca de modo significativo,
+ permitiendo un vuelo más lento que el de crucero. El dispositivo se inactiva replegándose de uno u otro modo durante el vuelo normal de crucero.'),
+(6,'Slat','Situados en el borde de ataque del ala, son dispositivos móviles que crean una ranura entre el borde de ataque del ala y el resto del plano.
+ A medida que el ángulo de ataque aumenta, el aire de alta presión situado
+ en la zona inferior del ala trata de llegar a la parte superior del ala,
+ dando energía de esta manera al aire en la parte superior y por tanto aumentando el
+ máximo ángulo de ataque que el avión puede alcanzar. Es un mecanismo de soplado que aporta cantidad de movimiento a la capa límite ayudando a vencer el gradiente adverso de presiones;
+ así se retrasa el desprendimiento de la corriente con respecto al aumento del ángulo de ataque.'),
+(7,'Flap de 3 partes interior','utilizado para el despegue y aterrizaje del avión'),
+(8,'Flap de 3 partes exterior','cumple la misma mision del flap de la parte interior'),
+(9,'Spoiler','destruye la sustentación del ala'),
+(10,'Spoiler-aerofreno','es un dispositivo que busca reducir la fuerza de sustentación de una aeronave. Los spoilers son placas montadas en la cara
+ superior de las alas de un avión (extradós), que pueden desplegarse hacia arriba modificando el flujo laminar
+«echándolo a perder» (spoil; en inglés, ‘echar a perder’).
+Con esto, el spoiler provoca una entrada en pérdida controlada sobre
+la porción del ala situada tras él, reduciendo notablemente la sustentación y aumentando la resistencia.'),
+(11, 'Ala', ' ala a un cuerpo aerodinámico formado por una estructura muy fuerte estructuralmente, compuesta por un perfil aerodinámico o perfil alar envolviendo a uno o más largueros y
+ que es capaz de generar una diferencia de presiones entre su cara superior (extradós)
+y su cara inferior (intradós) al desplazarse por el aire lo que produce
+la fuerza ascendente de sustentación que mantiene al avión en vuelo.
+El ala típica también utiliza el principio de acción y reacción generando una fuerza cuya componente vertical contrarresta al peso. En el caso particular de
+las alas para aviones supersónicos, el diseño está orientado a usar sólo este efecto de la "reacción" y se evita la sustentación
+aerodinámica. El ala compensará por tanto el peso del avión y a su vez generará una resistencia.'),
+(12,'Flecha ', 'El ala en flecha es una configuración alar común en los aviones de alta velocidad. La forma más común de ala en flecha es con los extremos de estas dirigidos atrás, en vez de formar un ángulo recto con el fuselaje'),
+(13,'Ojival ', 'tipo de ala usado en el avion '),
+(14,'Delta ', 'tipo de ala usado en el avion ');
